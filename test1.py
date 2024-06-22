d892108b8c71c7f831e385dda73093f8f736a5c6
Thinh Phat <153502922+phat-ht@users.noreply.github.com>
2024-06-22 21:40:47 +0700

    test1

--- test1.py
+++ test1.py
@@ -1,5 +1,4 @@
 hi = input("Enter a college class:")
 ha = input("Enter the adjustive:")
 hu = input("Enter an activity:")
-print(hi +" class was really "+ ha +" today. We learn how to " + hu+
-     " today in class. I can't wait for tomorrow's class!")
+print(hi +" class was really "+ ha +" today. We learn how to " + hu+ " today in class. I can't wait for tomorrow's class!")
